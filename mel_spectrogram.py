29f1ca3de0531c9241edeff3831c4f7e35c90043
ajayhanssen <andi.thoeni@gmail.com>
2025-02-26 07:59:56 +0100

    .h5, classification report

--- mel_spectrogram.py
+++ mel_spectrogram.py
@@ -3,9 +3,11 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-y, sr = librosa.load("test.wav")
+y, sr = librosa.load("samples/song_of_time/sample_0.wav", sr=44100)
+print(y.shape, sr)
 mel_spec = librosa.feature.melspectrogram(y=y, sr=sr)
 mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
+print(mel_spec_db.shape)
 
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(mel_spec_db, sr=sr, x_axis='time', y_axis='mel')
